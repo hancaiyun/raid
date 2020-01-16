e201325aa80d6e109439e8ce05270534c1ef4e45
hancaiyun <hancaiyun-it@bestpay.com.cn>
2020-01-16 14:04:15 +0800

    first commit

---
+++ src/test/java/com/nicehancy/raid/RaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicehancy.raid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
