6f95a15651ee5aa5522a48f99be025f76a35b75e
hancaiyun <hancaiyun-it@bestpay.com.cn>
2020-01-19 15:41:31 +0800

    页脚整理

--- src/main/java/com/nicehancy/raid/RaidApplication.java
+++ src/main/java/com/nicehancy/raid/RaidApplication.java
@@ -1,15 +1,18 @@
 package com.nicehancy.raid;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 服务启动类
  */
+@Slf4j
 @SpringBootApplication
 public class RaidApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RaidApplication.class, args);
+		log.info("服务已启动...");
 	}
 }
